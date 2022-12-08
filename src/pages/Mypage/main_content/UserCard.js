2dbe5c19a6dde212cca2c12004388e53d722d47c
wan <2jin_j@naver.com>
2022-12-09 06:30:54 +0900

    branch 문제 정리 필요, 회원정보변경 부분 추가

--- src/pages/Mypage/main_content/UserCard.js
+++ src/pages/Mypage/main_content/UserCard.js
@@ -1,21 +1,100 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCartShopping,
   faTicket,
   faPenToSquare,
+  faCircleExclamation,
 } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+
 // test
-import mydata from '../Mydata';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function UserCard() {
+  // redux
   let data = useSelector((state) => { return state })
+
+  // 회원정보변경 -> modal
+  function OpenModal() {
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const ValidPassword = () => {
+      // input에 입력한 password 값이 유효하면 페이지 이동
+      document.location.href('/')
+      // 이걸 onclick으로 걸어주기
+    }
+
+    // react-form-hook
+    const { register, handleSubmit, formState: { isSubmitting, isDirty, errors } } = useForm({ mode: "onChange" });
+    const onError = (error) => {
+      console.log(error);
+    };
+    const navigate = useNavigate();
+
+    
+    return (
+      <>
+        <span variant="primary" onClick={handleShow}>
+          회원정보 변경
+        </span>
+  
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <FontAwesomeIcon icon={faCircleExclamation} />
+              비밀번호 입력
+            </Modal.Title>
+
+          </Modal.Header>
+            <form
+              onSubmit={handleSubmit(async (data) => {
+                await new Promise((r) => setTimeout(r, 1000));
+                alert(JSON.stringify(data));
+                // form 제출 시 회원정보변경페이지로 이동
+                // 근데 이걸 id랑 pw 유효성 검사로? 토큰 확인
+                navigate('/mypage/myinfo')
+              }, onError)}
+            >
+            <Modal.Body>
+                <input
+                  type="password"
+                  placeholder="*********"
+                  aria-invalid={!isDirty ? undefined : errors.password ? "true" : "false"}
+                  {...register("password", {
+                    required: "비밀번호는 필수 입력입니다.",
+                    minLength: {
+                      value: 8,
+                      message: "8자리 이상 비밀번호를 사용하세요.",
+                    },
+                  })}
+                />
+                {errors.password && <small role="alert">{errors.password.message}</small>}
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="primary" onClick={handleClose} type="submit" disabled={isSubmitting}>
+                입력
+              </Button>
+            </Modal.Footer>
+          </form>
+        </Modal>
+      </>
+    );
+  }
+  
 
   return (
     <>
@@ -32,7 +111,7 @@ export default function UserCard() {
               {/* user에 따라 다른 주문 내역 */}
               {data.user[0].user_id}님
               <span style={{fontSize: '18px', marginLeft: '10px'}}>안녕하세요</span>
-              <span><Link to='/mypage/myinfo' style={{textDecoration: 'none', color: '#787878'}}>회원정보변경</Link></span>
+              <OpenModal />
             </span>
 
             <div className="d-flex position-absolute">
